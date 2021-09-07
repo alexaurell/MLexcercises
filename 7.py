5c7f12cf34aa72ed1e266d680480946b82532350
Alexander Aurell <alexaurell@gmail.com>
2021-09-07 14:17:49 +0200

    Solved exercise 7

---
+++ 7.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sklearn import linear_model
+
+df = pd.read_csv('College.csv')
+
+print(df.head())
+df.Private.replace(('Yes', 'No'), (1, 0), inplace=True)
+
+X = df[['Private', 'Accept', 'Enroll', 'Top10perc', 'Top25perc', 'F.Undergrad',
+        'P.Undergrad', 'Outstate', 'Room.Board', 'Books', 'Personal', 'PhD',
+        'Terminal', 'S.F.Ratio', 'perc.alumni', 'Expend', 'Grad.Rate']]
+y = df['Apps']
+
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
+
+print(regr.coef_)
